dc3441bbedc98a103462a315ddc035800537b341
Endrit Hashani <ehashani242@gmail.com>
2022-04-03 12:07:04 -1100

    commit

--- login.php
+++ login.php
@@ -46,7 +46,7 @@ if(isset($_POST['login'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./css/login.css?v">
+    <link rel="stylesheet" href="./css/login.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
